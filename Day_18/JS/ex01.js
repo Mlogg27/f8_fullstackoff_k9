aab9daf8df1fdf97dd7bf5af1e1a0b67d61150a6
Mlogg27 <mlogg271@gmail.com>
2024-06-19 16:13:37 +0700

    sua

---
+++ Day_18/JS/ex01.js
@@ -0,0 +1,6 @@
+var a=10;
+var b=20;
+a +=b;
+b = a -b;
+a-=b;
+console.log(`a là ${a} và b là ${b}`);
---
+++ Day_18/JS/ex04.js
@@ -0,0 +1,11 @@
+var a;
+var b;
+if (a*b < 0){
+    console.log(`2 số không cùng dấu`);
+}
+else if( a*b === 0) {
+    console.log(`One of them has zero value`);
+}
+else {
+    console.log(`2 số cùng dấu`);
+}
